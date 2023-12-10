3c615cd92beac9aa277656e5ecc775b21d3c2f2e
Abhijeet Chauhan <aschauhan1052002@gmail.com>
2023-12-10 20:21:06 +0530

    Added user model and registration

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+const userModel = require(`./users`)
+const localStrategy = require(`passport-local`)
+
+passport.use(new localStrategy(userModel.authenticate())); //This line is responsible for login functionality 
 
 router.get('/', function(req, res) {
   res.render('index', {footer: false});
@@ -28,5 +33,27 @@ router.get('/edit', function(req, res) {
 router.get('/upload', function(req, res) {
   res.render('upload', {footer: true});
 });
+
+router.post('/register', passport.authenticate('local',{
+  successRedirect:`/profile`,
+  failureRedirect:`/login`
+}),function(req, res) {
+  const userData = new userModel({
+    username: req.body.username,
+    name: req.body.name,
+    email: req.body.email,
+  })
+  userModel.register(userData,req.body.password) //This ig is provided by the plm plugin we added in the users.js and is responsible for registering the new user using passport and managing all password hash,store and fetch
+  .then(function(){
+    passport.authenticate(`local`)(req,res,function(){   //This line logs in the using local authentication 
+      res.redirect(`/profile`);
+    })
+  })
+});
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()) return next();
+  res.redirect('/login');
+}
 
 module.exports = router;
